7e79a4646aa84fccaa7c3dc1800d72de905bf110
silenc3502 <gcccompil3r@gmail.com>
2023-04-16 02:17:37 +0900

    feat: [이상훈] AWS S3 paginated list, delete, upload 완료

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,16 @@
 <template>
-  <hello-world />
+  <aws-sdk-file-upload/>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
-  import HelloWorld from '../components/HelloWorld.vue'
+  import AwsSdkFileUpload from '@/components/AwsSdkFileUpload.vue'
 
   export default Vue.extend({
     name: 'Home',
 
     components: {
-      HelloWorld,
+        AwsSdkFileUpload,
     },
   })
 </script>
